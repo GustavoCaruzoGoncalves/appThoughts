f697e3538f317698e3b56b7c09ae491c8470f65b
GustavoCaruzoGoncalves <gustavo.caruzo.goncalves@gmail.com>
2023-08-15 22:26:17 -0300

    Aula 1508 CRUD completo

--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -4,6 +4,12 @@ const ThoughtController = require('../controllers/ThoughtController')
 
 const checkAuth = require('../helpers/auth').checkAuth
 
+
+router.get('/add', checkAuth, ThoughtController.createThought)
+router.post('/add', checkAuth, ThoughtController.createThoughtSave)
+router.post('/remove', checkAuth, ThoughtController.removeThought)
+router.get('/edit/:id', checkAuth, ThoughtController.updateThought)
+router.post('/edit', checkAuth, ThoughtController.updateThoughtPost)
 router.get('/dashboard', ThoughtController.dashboard)
 router.get('/', ThoughtController.showThoughts)
 
